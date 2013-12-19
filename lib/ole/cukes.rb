6de52c47e5f3d8c9f73cfd12189a8e2271d3c247
Jain Waldrip <jkwaldri@iu.edu>
2013-12-19 12:29:30 -0500

    Initial OLE Repo Structure

--- lib/ole/cukes.rb
+++ lib/ole/cukes.rb
@@ -1,4 +1,8 @@
-require "ole/cukes/version"
+lib = File.join(File.dirname(__FILE__),'../../lib/')
+$:.unshift(lib) unless $:.include?(lib)
+
+require 'ole/cukes/version'
+require 'ole-qa-framework'
 
 module Ole
   module Cukes
