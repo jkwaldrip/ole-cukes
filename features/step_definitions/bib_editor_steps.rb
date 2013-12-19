472e47a5265c8999ce78bfdd6497534ca7387215
Jain Waldrip <jkwaldri@iu.edu>
2013-12-19 13:18:10 -0500

    Add author test steps

--- features/step_definitions/bib_editor_steps.rb
+++ features/step_definitions/bib_editor_steps.rb
@@ -17,9 +17,19 @@ Given /^I open a new bib record$/ do
   @bib_editor.open
 end
 
-When /^I add a title "(.*?)"$/ do |input|
+When /^I add a title "(.*?)"$/ do |title|
   @bib_editor.data_line.tag_field.when_present.set('245')
-  @bib_editor.data_line.data_field.when_present.set("|a#{input}")
+  @bib_editor.data_line.data_field.when_present.set("|a#{title}")
+end
+
+And /^I add an author "(.*?)"$/ do |author|
+  # We'll need to add an extra line to the bib editor first.
+  @bib_editor.data_line.add_button.click
+  @bib_editor.data_line.line_number += 1
+  @bib_editor.data_line.tag_field.wait_until_present
+  # Now we can add our author.
+  @bib_editor.data_line.tag_field.set('100')
+  @bib_editor.data_line.data_field.when_present.set("|a#{author}")
 end
 
 Then /^I can save the record$/ do
